6073bd58c1bbe5b7eb8084565b569e7797f75d41
sunkai <286190665@qq.com>
2021-01-19 22:37:43 +0800

    添加新项目

---
+++ springboot-hello/src/main/java/cn/com/sk/hello/controller/TestController.java
@@ -0,0 +1,15 @@
+package cn.com.sk.hello.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sk
+ * @title: TestController
+ * @projectName springboot-all
+ * @description: 测试controller
+ * @date 2021/1/199:34
+ */
+@RestController
+public class TestController {
+
+}
