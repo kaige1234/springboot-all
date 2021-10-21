5c6fae8674164fe3840e9623f825bc3e6d6f4634
sunkai <286190665@qq.com>
2021-10-21 18:51:47 +0800

    设计模式

---
+++ springboot-design-mode/src/main/java/cn/com/sk/designmode/controller/Test.java
@@ -0,0 +1,16 @@
+package cn.com.sk.designmode.controller;
+
+/**
+ * @author sunkai
+ * @title: Test
+ * @projectName springboot-all
+ * @description: TODO
+ * @date 2021/10/2116:41
+ */
+public class Test {
+
+    public static void main(String[] args){
+        String str = new String();
+
+    }
+}
