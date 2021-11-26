7af8d5c8ce3b090ba76f6de3a6c4b1779142096b
sunkai <286190665@qq.com>
2021-11-26 08:04:19 +0800

    hystrix open feign

---
+++ springboot-springcloud-hystrix-openfeign/src/main/java/cn/com/sunkai/hystrix/openfeign/springbootspringcloudhystrixopenfeign/SpringbootSpringcloudHystrixOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package cn.com.sunkai.hystrix.openfeign.springbootspringcloudhystrixopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringcloudHystrixOpenfeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringcloudHystrixOpenfeignApplication.class, args);
+	}
+
+}
---
+++ springboot-springcloud-hystrix-openfeign/src/test/java/cn/com/sunkai/hystrix/openfeign/springbootspringcloudhystrixopenfeign/SpringbootSpringcloudHystrixOpenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.sunkai.hystrix.openfeign.springbootspringcloudhystrixopenfeign;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringcloudHystrixOpenfeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
