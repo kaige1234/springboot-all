67412ba4aac3e7320832cc44684c095a2f400d8f
sunkai <286190665@qq.com>
2021-12-18 13:47:42 +0800

    k8s

---
+++ k8s/src/test/java/cn/com/sunkai/k8s/k8s/K8sApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.sunkai.k8s.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
